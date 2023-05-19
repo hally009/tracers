59adfa2520cdbac1db306f080a596505a994104e
hally009 <suhaili.polimedia@gmail.com>
2023-05-19 09:40:43 +0700

    tracer commit

---
+++ app/Http/Controllers/AktorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Aktor;
+use Illuminate\Http\Request;
+
+class AktorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('login');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Aktor  $aktor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Aktor $aktor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Aktor  $aktor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Aktor $aktor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Aktor  $aktor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Aktor $aktor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Aktor  $aktor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Aktor $aktor)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_05_18_115738_create_aktors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('aktors', function (Blueprint $table) {
+            $table->id();
+            $table->email();
+            $table->password();
+            $table->role();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('aktors');
+    }
+};
